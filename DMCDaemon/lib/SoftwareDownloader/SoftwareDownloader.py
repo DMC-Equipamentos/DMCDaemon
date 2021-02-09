4db75c0ca9c4c3e35c4cb03be00c2757001f400f
Danilo Lima <danilo.lima@dmcgroup.com.br>
2021-02-09 09:52:58 -0300

    Adicionadas configuraçoes em yaml

--- DMCDaemon/lib/SoftwareDownloader/SoftwareDownloader.py
+++ DMCDaemon/lib/SoftwareDownloader/SoftwareDownloader.py
@@ -2,8 +2,9 @@ import requests
 from config import * 
 import os
 
-
 def getFile(url, save_path):
+    if not os.path.exists(SOFTWARES_FOLDER):
+        os.makedirs(SOFTWARES_FOLDER)
     response = requests.get(url)
     open(os.path.join(SOFTWARES_FOLDER,save_path), 'wb').write(response.content)
 
---
+++ config.py
@@ -0,0 +1,17 @@
+import yaml
+
+with open('./config.yaml') as file:
+    settings_data = yaml.load(file, Loader=yaml.FullLoader)
+
+# Pasta para fazer download dos arquivos de software
+SOFTWARES_FOLDER = settings_data['SOFTWARES_FOLDER']
+
+DAEMON_PORT = settings_data['DAEMON_PORT']
+
+## Interfaces de programa ##
+
+MICROCHIP = settings_data['MICROCHIP']
+
+NINAB11 = settings_data['NINAB11']
+
+AVR = settings_data['AVR']
