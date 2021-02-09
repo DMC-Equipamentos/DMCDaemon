21002a5d6f37b83f3d9173c7e119a33b0ad8b430
Danilo Lima <danilo.lima@dmcgroup.com.br>
2021-02-09 10:50:46 -0300

    Correçoes de alguns nomes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from DMCDaemon.Server.serve import run_server
+# from DMCDaemon.Server.serve import run_server
 import config
+from DMCServiceDaemon.Server.serve import run_server
 
 run_server(config.DAEMON_PORT)
